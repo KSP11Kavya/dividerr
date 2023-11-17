40a90d93c05f345e32cd851c400f47e88366b177
kavyapandey <kavyapandeyksp11@gmail.com>
2023-11-17 19:47:05 +0530

    Divider

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,40 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(RunMyApp());
+}
+
+class RunMyApp extends StatelessWidget {
+  const RunMyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Divider Widget'),
+        ),
+        body: Column(
+          children: [
+            Text('Divider'),
+            Divider(
+              height: 100,
+              color: Colors.purple,
+              thickness: 2,
+            ),
+            Text('Divider'),
+            Divider(
+              height: 100,
+              color: Colors.purple,
+              thickness: 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
