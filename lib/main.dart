584a68696adb0d27a0b34694ad5d23a5c0752580
kavyapandey <kavyapandeyksp11@gmail.com>
2023-11-17 20:05:58 +0530

    Divider

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,47 @@
+// import 'package:flutter/material.dart';
+//
+// void main() {
+//   runApp(RunMyApp());
+// }
+//
+// class RunMyApp extends StatelessWidget {
+//   const RunMyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         primarySwatch: Colors.purple,
+//       ),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Divider Widget'),
+//         ),
+//         body: Column(
+//           children: [
+//             Text('Divider'),
+//             Divider(
+//               height: 100,
+//               color: Colors.purple,
+//               thickness: 2,
+//             ),
+//             Text('Divider'),
+//             Divider(
+//               height: 100,
+//               color: Colors.purple,
+//               thickness: 2,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+//You also give it padding from both sides
+// using the indent and endindent parameters.
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,14 +49,11 @@ void main() {
 }
 
 class RunMyApp extends StatelessWidget {
-  const RunMyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
+      theme: ThemeData(primarySwatch: Colors.purple
       ),
       home: Scaffold(
         appBar: AppBar(
@@ -24,14 +65,10 @@ class RunMyApp extends StatelessWidget {
             Divider(
               height: 100,
               color: Colors.purple,
-              thickness: 2,
-            ),
-            Text('Divider'),
-            Divider(
-              height: 100,
-              color: Colors.purple,
-              thickness: 2,
-            ),
+              thickness: 1,
+              indent: 20,
+              endIndent: 20,
+            )
           ],
         ),
       ),
